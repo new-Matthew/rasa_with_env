3cb16f6cd6a2f7f46ea80200635aef2897b590d4
matheus myenv <myenv@gmail.com>
2024-06-11 22:08:58 -0300

    buttons inicializado

--- actions/actions.py
+++ actions/actions.py
@@ -40,6 +40,17 @@ class ActionTravelInfo(Action):
          destiny_country = tracker.get_slot("destiny_country")
          origin_coin = tracker.get_slot("origin_coin")
 
+         if not origin_country:
+            dispatcher.utter_message(template="utter_ask_origin_country")
+            return [SlotSet("requested_slot", "origin_country")]
+
+         if not destiny_country:
+            dispatcher.utter_message(template="utter_ask_destiny_country")
+            return [SlotSet("requested_slot", "destiny_country")]
+
+
+
+
          dispatcher.utter_message(text=f"seu país de origem é {origin_country}, seu país de destino é {destiny_country}, com {origin_coin} você consegue comprar @destiny_coin (sem taxas de câmbio inclusas) ")
 
          return [AllSlotsReset(), SlotSet("requested_slot", "origin_country")]
